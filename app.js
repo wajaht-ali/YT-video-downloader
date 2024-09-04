eadd5938d466445e1c1b5d689a99156bade13a88
wajaht-ali <contact.chwajahat@gmail.com>
2024-09-04 12:32:17 +0500

    CORS conflict resolution

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const youtubeRoutes = require('./routes/yt_route.js');
+const express = require("express");
+const youtubeRoutes = require("./routes/yt_route.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,11 +7,19 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON
 app.use(express.json());
 
+// cors options
+app.use(
+  cors({
+    origin: "https://yt-video-downloader-production.up.railway.app",
+    method: ["POST", "GET", "PUT", "DELETE"],
+    withCredentials: true,
+  })
+);
 // Routes
-app.use('/', (req, res) => {
-    res.send('Server is running');
+app.use("/", (req, res) => {
+  res.send("Server is running");
 });
-app.use('/download', youtubeRoutes);
+app.use("/download", youtubeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
